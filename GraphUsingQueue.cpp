ea2c02e3dea8f087094aade5ceada8e759f4e0fb
Ranajit <18ranajit2005@gmail.com>
2023-11-07 00:48:35 +0530

    er

--- GraphUsingQueue.cpp
+++ GraphUsingQueue.cpp
@@ -12,48 +12,48 @@ public:
         front = rear = -1;
         arr = new int[size];
     }
-    bool isFull();
-    bool isEmpty();
-    void EnQ(int );
-    int DeQ();
-    void display();
+    bool isFull(Q *q);
+    bool isEmpty(Q *q);
+    void EnQ(Q *q,int );
+    int DeQ(Q *q);
+    void display(Q *q);
 };
 
-bool Q :: isFull(){
-    if(rear == size-1){
+bool Q :: isFull(Q *q){
+    if(q->rear == q->size-1){
         cout<<"Queue is full";
         return 1;
     }   
     return 0;
 }
 
-bool Q :: isEmpty(){
-    if(rear == front){
+bool Q :: isEmpty(Q *q){
+    if(q->rear == q->front){
         cout<<"Queue is empty";
         return 1;
     }
     return 0;
 }
 
-void Q :: EnQ(int data){
-    if(!isFull()){
-        rear ++;
-        arr[rear] = data;
+void Q :: EnQ(Q *q,int data){
+    if(!isFull(q)){
+        q->rear ++;
+        arr[q->rear] = data;
     }
     return;
 }
 
-int Q :: DeQ(){
-    if(!isEmpty()){
-        front++;
-        int x = arr[front];
+int Q :: DeQ(Q *q){
+    if(!isEmpty(q)){
+        q->front++;
+        int x = arr[q->front];
         return x;
     }
     return -1;
 }
 
-void Q :: display(){
-    for (int i = front + 1; i <= rear; i++){
+void Q :: display(Q *q){
+    for (int i = q->front + 1; i <= q->rear; i++){
         cout << arr[i] << " ";
     }
     cout << endl;
@@ -61,13 +61,13 @@ void Q :: display(){
 
 int main(){
     Q rd(5);
-    rd.EnQ(1);
-    rd.EnQ(2);
-    rd.EnQ(3);
-    rd.EnQ(4);
-    rd.EnQ(5);
-    rd.display();
-    rd.DeQ();
-    rd.display();
+    rd.EnQ(&rd,1);
+    rd.EnQ(&rd,2);
+    rd.EnQ(&rd,3);
+    rd.EnQ(&rd,4);
+    rd.EnQ(&rd,5);
+    rd.display(&rd);
+    rd.DeQ(&rd);
+    rd.display(&rd);
     return 0;
 }
