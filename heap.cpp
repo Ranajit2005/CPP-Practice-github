7d4c42f8ba816d3e9cdc8ee17d63f4d7157e8e71
Ranajit <18ranajit2005@gmail.com>
2023-12-07 01:04:33 +0530

    wrong delete'
    ef
    e
    exit
    ''

--- heap.cpp
+++ heap.cpp
@@ -11,6 +11,24 @@ void InsertForMaxHeap(int arr[],int n){
     arr[i] = temp;
 }
 
+void Delete(int arr[],int n){   //Delete is possible only from rrot
+    int x,i,j;
+    x = arr[n];     //store root at a variable
+    arr[1] = arr[n];    //Delete root
+    i = 1;
+    j = 2 * i;
+    while(j<n-1){
+        if(arr[j+1]>arr[j])     j = j+1;    //Check which is bigger bigger between left or right child
+        if(arr[i]<arr[j]){      //Check whichn is bigger between parent and child
+            swap(arr[i],arr[j]);
+            i=j;
+            j=2*i;
+        }
+        else    break;
+    }
+    arr[n] = x;
+}
+
 int main(){
     int n;
     cout<<"Give the size of the array : ";
@@ -25,8 +43,17 @@ int main(){
         // cout<<endl;
     }
 
+    cout<<"After insert the array is : ";
+    for(int i=1;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
 
+    for(int i=size;i>1;i--){
+        Delete(arr,i);
+    }
 
+    cout<<"The sorted array is : ";
     for(int i=1;i<size;i++){
         cout<<arr[i]<<" ";
     }
