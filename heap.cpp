2d791d54771c3a8b6051ddb8c7805fbf74206c9e
Ranajit <18ranajit2005@gmail.com>
2023-12-06 22:37:20 +0530

    heap

---
+++ heap.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int n;
+    int arr[n];
+    cout<<"Give the size of the array : ";
+    cin>>n;
+    cout<<endl;
+    arr[0]=0;   //becaue here we don't use index 0
+    for(int i=1;i<n;i++){   //so the loop is start from 1
+        cout<<"Give the element of the array in index at "<<i<<" is : ";
+        cin>>arr[i];
+        cout<<endl;
+    }
+return 0;
+}
