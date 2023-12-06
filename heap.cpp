1117db3b426a10edcb2b344f345792fc81ae88fd
Ranajit <18ranajit2005@gmail.com>
2023-12-06 23:18:02 +0530

    HeapInsert

--- heap.cpp
+++ heap.cpp
@@ -1,24 +1,34 @@
 #include<iostream>
 using namespace std;
 
+void InsertForMaxHeap(int arr[],int n){
+    int temp,i=n;
+    temp = arr[n];
+    while(i>1 && temp>arr[i/2]){
+        arr[i] = arr[i/2];
+        i = i/2;
+    }
+    arr[i] = temp;
+}
 
 int main(){
     int n;
-    int arr[n];
     cout<<"Give the size of the array : ";
     cin>>n;
-    cout<<endl;
-    arr[0]=0;   //becaue here we don't use index 0
-    for(int i=1;i<n;i++){   //so the loop is start from 1
-        cout<<"Give the element of the array in index at "<<i<<" is : ";
+    int size = n + 1;   //for 1 base index
+    int arr[size];
+    arr[0]=0;   //because here we don't use index 0
+    for(int i=1;i<size;i++){    //start from index 1
+        cout<<"Give the element of the array at index "<<i<<" : ";
         cin>>arr[i];
-        cout<<endl;
+        InsertForMaxHeap(arr,i);
+        // cout<<endl;
     }
 
 
-    for(int i=1;i<n;i++){
+
+    for(int i=1;i<size;i++){
         cout<<arr[i]<<" ";
     }
-    cout<<endl;
 return 0;
 }
