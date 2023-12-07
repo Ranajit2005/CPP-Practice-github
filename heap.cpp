8f7cc18541cc6d46a7a037bf93bc6dec257584f2
Ranajit <18ranajit2005@gmail.com>
2023-12-07 20:52:00 +0530

    Delete of heap

--- heap.cpp
+++ heap.cpp
@@ -20,14 +20,24 @@ void InsertForMinHeap(int arr[],int n){
     }
     arr[i] = temp;
 }
+	
+	// void heapify(int i,int n){
+	// 	int largest = i;
+	// 	int left = 2*i , right = left + 1;
+	// 	if( left < n && v[left] > v[largest] )  largest = left;
+	// 	if( right < n && v[right] > v[largest] )  largest = right;
+	// 	if( largest != i ){
+	// 		swap( v[largest] , v[i] );
+	// 		heapify( largest , n );	
+	// 	}
 
 void Delete(int arr[],int n){   //Delete is possible only from rrot
     int x,i,j;
-    x = arr[n];     //store root at a variable
+    x = arr[1];     //store root at a variable
     arr[1] = arr[n];    //Delete root
     i = 1;
     j = 2 * i;
-    while(j<n-1){
+    while(j<=n-1){
         if(arr[j+1]>arr[j]){    //Check which is bigger bigger between left or right child
         j = j+1;   
         }
@@ -62,13 +72,17 @@ int main(){
     }
     cout<<endl;
 
-    for(int i=size;i>1;i--){
-        Delete(arr,i);
-    }
-
+    // for(int i=size-1;i>2;i--){
+    //     Delete(arr,i);
+    // }
+    Delete(arr,5);
+    Delete(arr,4);
+    Delete(arr,3);
+    // // Delete(arr,2);
+    // // Delete(arr,1);
     cout<<"The sorted array is : ";
     for(int i=1;i<size;i++){
         cout<<arr[i]<<" ";
     }
-return 0;
+    return 0;
 }
