7a88465ee1ff27accf9e1f1e86d03a8cb2622629
Ranajit <18ranajit2005@gmail.com>
2024-01-20 00:45:39 +0530

    complex3

--- Complex.cpp
+++ Complex.cpp
@@ -16,7 +16,7 @@ public:
     }
     void display()
     {
-        cout << "The number is : " << real << "+ " << img << "i" << endl;
+        cout << "The number is : " << real << " + " << img << "i" << endl;
     }
 };
 
@@ -28,15 +28,41 @@ void Complex ::input()
     cin >> img;
 }
 
+class complex1
+{
+    int real, img;
+
+public:
+    complex1(int c, int d)
+    {
+        real = c;
+        img = d;
+    }
+    complex1 add(complex1 obj)
+    {
+        complex1 temp(0, 0);
+        temp.real = real + obj.real;
+        temp.img = img + obj.img;
+        return temp;
+    }
+    void display()
+    {
+        cout << "The number is : " << real << " + " << img << "i" << endl;
+    }
+};
+
 int main()
 {
-    Complex com1, com2, res;
-    cout << "Enter the 1st complex number : " << endl;
-    com1.input();
-    cout << "Enter the 2nd complex number : " << endl;
-    com2.input();
-    res = com1 + com2;
-    res.display();
+    // Complex com1, com2, res;
+    // cout << "Enter the 1st complex number : " << endl;
+    // com1.input();
+    // cout << "Enter the 2nd complex number : " << endl;
+    // com2.input();
+    // res = com1 + com2;
+    // res.display();
 
+    complex1 com1(3,5), com2(7,9);
+    complex1 sum = com1.add(com2);
+    sum.display();
     return 0;
 }
