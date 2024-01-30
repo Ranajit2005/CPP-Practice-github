dae77a456a5df6dd14dfbca45054f89bad358bc9
Ranajit <18ranajit2005@gmail.com>
2024-01-31 04:13:20 +0530

    link3

--- Sem3/SinLinkList.cpp
+++ Sem3/SinLinkList.cpp
@@ -16,13 +16,27 @@ public:
     Linklist(){head = NULL;}
     void InsertFirst(int x){
         Node* t = new Node(x);
-        // if(head == NULL){
-        //     head = t;
-        // }else{
             t->next = head;
-            head = t;
-        // }
-        
+            head = t;  
+    }
+
+    void InsertLast(int x){
+        Node* p = head;
+        Node* t = new Node(x);
+        while(p->next!=NULL){
+            p=p->next;
+        }
+        p->next = t;
+    }
+
+    void InsertPos(int x,int pos){
+        Node* p = head;
+        Node *t = new Node(x);
+        for(int i=1;i<pos - 1 && p;i++){
+            p=p->next;
+        }
+        t->next = p->next;
+        p->next = t;
     }
 
     void Display(){
@@ -36,13 +50,26 @@ public:
 
 int main(){
     Linklist li;
+    li.InsertFirst(10);
     li.InsertFirst(20);
     li.InsertFirst(30);
-    li.InsertFirst(40);
-    li.InsertFirst(50);
-    li.InsertFirst(10);
+    // li.InsertFirst(40);
+    // li.InsertFirst(50);
 
     li.Display();
-    cout<<"End";
+    cout<<endl;
+
+    li.InsertLast(15);
+    li.InsertLast(25);
+    li.Display();
+    cout<<endl;
+
+    li.InsertPos(17,3);
+    li.Display();
+    cout<<endl;
+    li.InsertPos(33,5);
+
+    li.Display();
+    cout<<endl<<"End";
     return 0;
 }
