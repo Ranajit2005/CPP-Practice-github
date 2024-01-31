26fd08496a5773df18c349018c14a8f432b1144d
Ranajit <18ranajit2005@gmail.com>
2024-01-31 23:14:23 +0530

    link7

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -2,16 +2,19 @@
 using namespace std;
 
 void BubbleSort(int arr[],int n){
+    int count = 0;
     for(int i=0;i<n-1;i++){
         bool flag = 0;
         for(int j=0;j<n-1-i;j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
                 flag = 1;
+                count++;
             }
         }
         if(flag == 0)   break;
     }
+    cout<<endl<<count<<endl;
 }
 
 int main(){
