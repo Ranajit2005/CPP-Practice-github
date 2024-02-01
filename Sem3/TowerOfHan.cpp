ed60d09565088e2ad153b7c8d5cdad76b7887baf
Ranajit <18ranajit2005@gmail.com>
2024-02-01 14:01:51 +0530

    jyegd

--- Sem3/TowerOfHan.cpp
+++ Sem3/TowerOfHan.cpp
@@ -8,10 +8,12 @@ void TOH(int start,int end,char sour,char help,char dest){
         TOH(start,end-1,sour,dest,help);
         cout<<"move "<<end<<" from "<<sour<<" to "<<dest<<endl;
         TOH(start,end-1,help,sour,dest);
+    //     count++;
+    //     cout<<"Moving "<<count<<endl;
     }
 }
 
 int main(){
-    TOH(1,3,'A','B','C');
+    TOH(1,5,'A','B','C');
     return 0;
 }
