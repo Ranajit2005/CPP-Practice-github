1889dcada26cb418b8169778ece3aa938faa7ef5
Ranajit <18ranajit2005@gmail.com>
2024-02-01 13:29:07 +0530

    fack

--- Sem3/fac.cpp
+++ Sem3/fac.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
 using namespace std;
 
-int 
+int fac(int n){
+    if(n == 1){
+        return 1;
+    }
+    return n * fac(n-1);
+}
 
 int main(){
 
+    cout<<fac(6);
     return 0;
 }
