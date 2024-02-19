a98ef5aca34c1c5d1bea6af256d8dd87fd699204
Ranajit <18ranajit2005@gmail.com>
2024-02-19 23:56:54 +0530

    ef

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,14 +1,4 @@
-cin>>n;
-    // cout<<endl;
-    // arr[0]=0;   //becaue here we don't use index 0
-    // for(int i=1;i<n;i++){   //so the loop is start from 1
-    //     cout<<"Give the element of the array in index at "<<i<<" is : ";
-    //     cin>>arr[i];
-    //     cout<<endl;
-    // }
-
-
-    // for(int i=1;i<n;i++){
-    //     cout<<arr[i]<<" ";
-    // }
-    // cout<<endl;
+Node(int value){
+        data=value;
+        next=NULL;
+    }
