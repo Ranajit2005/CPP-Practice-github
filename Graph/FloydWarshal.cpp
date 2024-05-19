ac9e47598e2b36156fafedc38203dff32c6bab8f
Ranajit2005 <18ranajit2005@gmail.com>
2024-05-20 02:35:30 +0530

    Floyd Warshall

--- Graph/FloydWarshal.cpp
+++ Graph/FloydWarshal.cpp
@@ -26,7 +26,7 @@ int main(){
 
     for(int k=1;k<=v;k++){
         for(int i=1;i<=v;i++){
-            for(int j=0;j<=v;j++){
+            for(int j=1;j<=v;j++){
                 dist[i][j] = min(dist[i][j],dist[i][k]+dist[k][j]);
             }
         }
