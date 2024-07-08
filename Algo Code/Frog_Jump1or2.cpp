e69d4fc6d28854a82a90c8acadd3d877f072aafd
Ranajit2005 <18ranajit2005@gmail.com>
2024-07-09 00:32:31 +0530

    Frog Jump Problem Complete

--- Algo Code/Frog_Jump1or2.cpp
+++ Algo Code/Frog_Jump1or2.cpp
@@ -2,7 +2,36 @@
 using namespace std;
 // Frog can jump at a time for one or two steps
 
+int FromJump(int n,int cost[]){
+    int DpArray[n] = {0};
+    DpArray[0] = 0;
+
+    for(int i = 1;i<n;i++){
+        int OneJump = DpArray[i-1] + abs(cost[i] - cost[i-1]);
+        int TwoJump = INT16_MAX;
+        if(i>1)     TwoJump = DpArray[i-2] + abs(cost[i] - cost[i-2]);
+
+        //Update Dp Array
+        DpArray[i] = min(OneJump,TwoJump);
+    }
+
+    return DpArray[n-1];
+
+}
+
 int main(){
+    int step;
+    cout<<"Give the total number of steps : ";
+    cin>>step;
+
+    int cost[step];
+    for(int i=0;i<step;i++){
+        cout<<"Give energy between "<<i<<" and "<<i+1<<"step : ";
+        cin>>cost[i];
+    }
+
+    int ans = FromJump(step,cost);
+    cout<<"The minimum energy required is : "<<ans<<endl;
 
     return 0;
 }
