f6270e7fd74594f9c5eb0196ca2c3cb92abd71bd
Ranajit2005 <18ranajit2005@gmail.com>
2025-10-09 14:10:36 +0530

    sign and unsigned bit

---
+++ Number Theory/signANDunsignNumber.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<INT_MAX<<endl;
+
+    int a = (1LL << 32) - 1; //2^31
+    cout<<a<<endl;
+
+    unsigned int b = (1LL << 32) - 1;
+    cout << b <<endl;
+
+    // one bit is reserve for sign (positive or negetive )
+
+    return 0;
+}
